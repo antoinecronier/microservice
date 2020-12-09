7058b0e73650430c051cfcc9b2e48b31fe7c91df
Antoine Cronier <antoine.cronier@tactfactory.com>
2020-12-09 14:35:22 +0100

    link items

---
+++ microservice-currencyconvertionservice/src/main/java/com/tactfactory/microservice/currencyconvertionservice/MicroserviceCurrencyconvertionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tactfactory.microservice.currencyconvertionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCurrencyconvertionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCurrencyconvertionserviceApplication.class, args);
+	}
+
+}
---
+++ microservice-currencyconvertionservice/src/test/java/com/tactfactory/microservice/currencyconvertionservice/MicroserviceCurrencyconvertionserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tactfactory.microservice.currencyconvertionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCurrencyconvertionserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
