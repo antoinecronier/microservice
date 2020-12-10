d5cbab70a033f6a7a0e3a1c2dcef7e75209d4e5a
Antoine Cronier <antoine.cronier@tactfactory.com>
2020-12-10 11:15:40 +0100

    eureka discovery enabled

--- microservice-currencyconvertionservice/src/main/java/com/tactfactory/microservice/currencyconvertionservice/MicroserviceCurrencyconvertionserviceApplication.java
+++ microservice-currencyconvertionservice/src/main/java/com/tactfactory/microservice/currencyconvertionservice/MicroserviceCurrencyconvertionserviceApplication.java
@@ -2,10 +2,12 @@ package com.tactfactory.microservice.currencyconvertionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.tactfactory.microservice.currencyconvertionservice.feigns")
+@EnableDiscoveryClient
 public class MicroserviceCurrencyconvertionserviceApplication {
 
 	public static void main(String[] args) {
