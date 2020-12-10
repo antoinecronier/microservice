a37b3ca553de0a98143b09ecd28728c6e385b21f
Antoine Cronier <antoine.cronier@tactfactory.com>
2020-12-10 11:01:44 +0100

    Reset .gitignore + feign load balancing without eureka failed

---
+++ microservice-limitsservice/src/test/java/com/tactfactory/microservice/limitsservice/MicroserviceLimitsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tactfactory.microservice.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceLimitsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
