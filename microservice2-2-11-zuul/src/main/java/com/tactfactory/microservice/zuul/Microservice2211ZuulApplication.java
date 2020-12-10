f3bf818eb7a73648da226bfcfbc6b9812df36895
Antoine Cronier <antoine.cronier@tactfactory.com>
2020-12-10 13:52:53 +0100

    Add bean filter

--- microservice2-2-11-zuul/src/main/java/com/tactfactory/microservice/zuul/Microservice2211ZuulApplication.java
+++ microservice2-2-11-zuul/src/main/java/com/tactfactory/microservice/zuul/Microservice2211ZuulApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -14,4 +16,8 @@ public class Microservice2211ZuulApplication {
 		SpringApplication.run(Microservice2211ZuulApplication.class, args);
 	}
 
+	@Bean
+	public ZuulLoggingFilter loggingFilter() {
+		return new ZuulLoggingFilter();
+	}
 }
