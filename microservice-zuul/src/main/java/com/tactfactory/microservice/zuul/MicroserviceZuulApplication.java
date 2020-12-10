b047d3930935062bc0af4fa41fcade4b4f33e355
Antoine Cronier <antoine.cronier@tactfactory.com>
2020-12-10 11:44:06 +0100

    zuul not working

---
+++ microservice-zuul/src/main/java/com/tactfactory/microservice/zuul/MicroserviceZuulApplication.java
@@ -0,0 +1,17 @@
+package com.tactfactory.microservice.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class MicroserviceZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceZuulApplication.class, args);
+	}
+
+}
---
+++ microservice-zuul/src/main/java/com/tactfactory/microservice/zuul/filters/ZuulLogingFilter.java
@@ -0,0 +1,41 @@
+package com.tactfactory.microservice.zuul.filters;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+@Component
+public class ZuulLogingFilter extends ZuulFilter {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() {
+		// getting the current HTTP request that is to be handle
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		// printing the detail of the request
+		logger.info("request -> {} request uri-> {}", request, request.getRequestURI());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
