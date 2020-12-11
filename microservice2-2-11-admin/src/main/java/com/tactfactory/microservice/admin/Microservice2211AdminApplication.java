657f8bc9e501ea3fd9b5526fd742c2c8f9dccdc3
Antoine Cronier <antoine.cronier@tactfactory.com>
2020-12-11 10:05:07 +0100

    add admin

---
+++ microservice2-2-11-admin/src/main/java/com/tactfactory/microservice/admin/Microservice2211AdminApplication.java
@@ -0,0 +1,21 @@
+package com.tactfactory.microservice.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
+/**
+ * Refer to https://codecentric.github.io/spring-boot-admin/1.4.3/#set-up-admin-server
+ * */
+public class Microservice2211AdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservice2211AdminApplication.class, args);
+	}
+
+}
---
+++ microservice2-2-11-admin/src/test/java/com/tactfactory/microservice/admin/Microservice2211AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tactfactory.microservice.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservice2211AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
